321d9dfcd41caecdfb55919952b56721cacc5bbf
Shriya Kethireddy <skethire@umich.edu>
2024-02-18 13:27:22 -0500

    committing

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -6,7 +6,11 @@ URLs include:
 """
 import flask
 import insta485
+from flask_socketio import SocketIO
+import datetime
 
+app2 = flask.Flask(__name__)
+socketio = SocketIO(app2)
 
 @insta485.app.route('/')
 def show_index():
@@ -14,3 +18,10 @@ def show_index():
     context = {}
     return flask.render_template("index.html", **context)
 
+@socketio.on('connect')
+def handle_connect():
+    # Emit the current time to the connected client
+    socketio.emit('update_time', {'time': str(datetime.datetime.now())})
+
+if __name__ == '__main__':
+    socketio.run(app2)

28cdfd09f16f24ad96500f9ac296b1f5be81264e
Shriya Kethireddy <skethire@umich.edu>
2024-03-16 18:02:21 -0400

    added a kivy version

--- test2/model.py
+++ test2/model.py
@@ -3,13 +3,26 @@ kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
+class MainScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.add_widget(Label(text='Increase Temp'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
-
-
+        layout = BoxLayout(orientation='vertical')
+        button = Button(text='User Name')
+        layout.add_widget(button)
+        return layout
 if __name__ == '__main__':
     MyApp().run()

5b2f2ca507edd853fe0ae44fdcc9c23f7a741d9e
npkamath <npkamath@umich.edu>
2024-04-07 14:08:16 -0400

    more button

--- test2/model.py
+++ test2/model.py
@@ -5,53 +5,51 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.clock import Clock
+import smbus2
+import time
+
+class SHT30:
+    def __init__(self, bus_number=1):
+        self.bus = smbus2.SMBus(bus_number)
+        self.address = 0x44  # SHT30 I2C address
+
+    def read_temperature_humidity(self):
+        # Send measurement command, 0x2C(44), 0x06(06) High repeatability measurement
+        self.bus.write_i2c_block_data(self.address, 0x2C, [0x06])
+        time.sleep(0.5)  # Wait for the measurement to complete
+
+        # Read 6 bytes of data: temperature and humidity
+        data = self.bus.read_i2c_block_data(self.address, 0x00, 6)
+
+        # Convert the data
+        cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+
+        return cTemp, humidity
 
 class MyApp(App):
     def build(self):
+        self.sensor = SHT30()
+
         # Main layout
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
-        # Mattress Temperature Section
-        self.mattress_temp = 30.0  # Initial mattress temperature
-        mattress_layout = BoxLayout(orientation='horizontal', size_hint_y=None, height='48dp')
-        mattress_label = Label(text='Mattress Temperature: ')
-        self.mattress_temp_label = Label(text=f'{self.mattress_temp}°C')
-        mattress_layout.add_widget(mattress_label)
-        mattress_layout.add_widget(self.mattress_temp_label)
-        
-        layout.add_widget(mattress_layout)
+        # Sensor Temperature and Humidity Section
+        self.sensor_temp_label = Label(text='Sensor Temperature: -- °C')
+        self.sensor_humidity_label = Label(text='Sensor Humidity: -- %RH')
+        layout.add_widget(self.sensor_temp_label)
+        layout.add_widget(self.sensor_humidity_label)
 
-        # Button Section
-        # Creating a new layout for buttons to be in the center-ish of the screen
-        buttons_layout = BoxLayout(orientation='horizontal', size_hint=(None, None), width='200dp', height='100dp', pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        up_button = Button(text='↑', size_hint=(None, None), size=('100dp', '100dp'))
-        up_button.bind(on_press=self.increase_mattress_temp)
-        down_button = Button(text='↓', size_hint=(None, None), size=('100dp', '100dp'))
-        down_button.bind(on_press=self.decrease_mattress_temp)
-        buttons_layout.add_widget(up_button)
-        buttons_layout.add_widget(down_button)
-        
-        # Placing the buttons layout in the center of the main layout
-        layout.add_widget(buttons_layout)
-
-        # Human Temperature Section
-        human_layout = BoxLayout(orientation='horizontal', size_hint_y=None, height='48dp')
-        human_label = Label(text='Human Temperature: ')
-        human_temp_label = Label(text='30°C')  # Placeholder for human temperature
-        human_layout.add_widget(human_label)
-        human_layout.add_widget(human_temp_label)
-
-        layout.add_widget(human_layout)
+        # Update the sensor readings every 2 seconds
+        Clock.schedule_interval(self.update_sensor_readings, 2)
 
         return layout
 
-    def increase_mattress_temp(self, instance):
-        self.mattress_temp += 0.5
-        self.mattress_temp_label.text = f'{self.mattress_temp}°C'
-
-    def decrease_mattress_temp(self, instance):
-        self.mattress_temp -= 0.5
-        self.mattress_temp_label.text = f'{self.mattress_temp}°C'
+    def update_sensor_readings(self, dt):
+        cTemp, humidity = self.sensor.read_temperature_humidity()
+        self.sensor_temp_label.text = f'Sensor Temperature: {cTemp:.2f} °C'
+        self.sensor_humidity_label.text = f'Sensor Humidity: {humidity:.2f} %RH'
 
 if __name__ == '__main__':
     os.environ['DISPLAY'] = ':0'
